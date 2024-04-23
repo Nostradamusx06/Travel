867e3d518d4fac360c5b37573b87d310623910c8
Лев Чернин <cherninleva@gmail.com>
2024-04-23 20:43:27 +0300

    feat: desktop, tablet and mobile versions of training block

---
+++ source/js/training-swiper.js
@@ -0,0 +1,45 @@
+import Swiper from 'swiper';
+import { Navigation } from 'swiper/modules';
+
+(function () {
+  const initTrainingSwiper = () => {
+    const swiperTraining = new Swiper('.training__swiper', {
+      autoHeight: true,
+      modules: [Navigation],
+      speed: 600,
+
+      navigation: {
+        nextEl: '.training__button--next',
+        prevEl: '.training__button--prev',
+        disabledClass: 'training__button--disabled',
+      },
+
+      keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+      },
+
+      breakpoints: {
+        320: {
+          allowTouchMove: true,
+          slidesPerView: 1,
+          initialSlide: 2,
+        },
+        768: {
+          allowTouchMove: true,
+          slidesPerView: 3,
+          spaceBetween: 20,
+        },
+        1440: {
+          allowTouchMove: false,
+          slidesPerView: 4,
+          spaceBetween: 20,
+        },
+      },
+    });
+
+    return swiperTraining;
+  };
+
+  initTrainingSwiper();
+})();
